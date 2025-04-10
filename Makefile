e76b01cffe579bc50949d0681278c16bddfd5980
Kevin McManamon <kevin.mcmanamon@n-able.com>
2025-04-10 09:25:57 +0100

    uncomment test file

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 build :
 	rm -r env
-	python3 -m venv env
+	python3.12 -m venv env
 	. env/bin/activate && pip install -r requirements.txt
 
 test:
--- test_readme.py
+++ test_readme.py
@@ -1,10 +1,10 @@
-# import pytest
-#
-#
-# class TestReadMe:
-#
-#     def test_the_bug_does_not_appear_in_readme(self):
-#         with open('README.md') as readme:
-#             if 'THE BUG' in readme.read():
-#                 pytest.fail("bug found")
-#
+import pytest
+
+
+class TestReadMe:
+
+    def test_the_bug_does_not_appear_in_readme(self):
+        with open('README.md') as readme:
+            if 'THE BUG' in readme.read():
+                pytest.fail("bug found")
+
